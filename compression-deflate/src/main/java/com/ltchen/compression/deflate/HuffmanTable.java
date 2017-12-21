51ac7e4b403b673e6842a33b886492e38ec4b2fc
ltchen <loupipalien@gmail.com>
2017-12-22 03:28:03 +0800

    霍夫曼表

---
+++ compression-deflate/src/main/java/com/ltchen/compression/deflate/HuffmanTable.java
@@ -0,0 +1,81 @@
+package com.ltchen.compression.deflate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : ltchen
+ * @date : 2017/12/22
+ * @desc : 霍夫曼树
+ */
+public class HuffmanTable {
+
+    /**
+     * 默认的霍夫曼码表
+     * 见 RFC 1951, 3.2.6 章节 (https://www.ietf.org/rfc/rfc1951.txt)
+     */
+    public static final HuffmanTable LIT;
+    public static final HuffmanTable DIST;
+    static {
+        // 生成固定的字母/长度码
+        LIT = new HuffmanTable(286);
+        int nextCode = 0;
+        for (int i = 256; i <= 279; i++) {
+            LIT.codes[i] = nextCode++;
+            LIT.codeLens[i] = 7;
+        }
+        // 0010111 -> 00110000
+        nextCode <<= 1;
+        for (int i = 0; i <= 143; i++) {
+            LIT.codes[i] = nextCode++;
+            LIT.codeLens[i] = 8;
+        }
+        // 286,287 不会被用到
+        for (int i = 280; i <= 285; i++) {
+            LIT.codes[i] = nextCode++;
+            LIT.codeLens[i] = 9;
+        }
+        // 11000101 -> 110010000
+        nextCode = (nextCode + 2) << 1;
+        for (int i = 144; i < 255; i++) {
+            LIT.codes[i] = nextCode++;
+            LIT.codeLens[i] = 9;
+        }
+
+        // 生成固定的距离码
+        DIST = new HuffmanTable(30);
+        for (int i = 0; i <= 29 ; i++) {
+            DIST.codes[i] = i;
+            DIST.codeLens[i] = 5;
+        }
+    }
+
+    /**
+     * 码的数组
+     */
+    public int[] codes;
+
+    /**
+     * 码长度的数组
+     */
+    public int[] codeLens;
+
+    /**
+     * 构造霍夫曼表
+     * @param codeCount 码的总数
+     */
+    public HuffmanTable(int codeCount) {
+        codes = new int[codeCount];
+        codeLens = new int[codeCount];
+    }
+
+    public static List<Integer> packCodeLens(int[] litCodeLens, int[] distCodeLens) {
+        List<Integer> lengths = new ArrayList<Integer>();
+        return lengths;
+    }
+
+    private static void pack(List<Integer> lengths, int[] codeLens) {
+
+    }
+
+}
--- compression-deflate/src/main/java/com/ltchen/compression/deflate/HuffmanTree.java
+++ compression-deflate/src/main/java/com/ltchen/compression/deflate/HuffmanTree.java
@@ -1,9 +1,6 @@
 package com.ltchen.compression.deflate;
 
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * @author : ltchen
@@ -13,9 +10,9 @@ import java.util.TreeMap;
 public class HuffmanTree {
 
     /**
-     * 码的总个数
+     * 码的总数
      */
-    private int codesCount;
+    private int codeCount;
 
     /**
      *
@@ -39,13 +36,13 @@ public class HuffmanTree {
      */
     public HuffmanTree(int[] freqs, int limit) {
         // 初始化
-        codesCount = freqs.length;
+        codeCount = freqs.length;
         depthMap = new TreeMap<Integer,List<LeafNode>>();
         maxDepth = 0;
 
         // 初始化叶子节点
         PriorityQueue<Node> queue = new PriorityQueue<Node>();
-        for (int i = 0; i < codesCount; i++) {
+        for (int i = 0; i < codeCount; i++) {
             if (freqs[i] > 0) {
                 // i 作为值, freqs[i] 作为权重
                 queue.add(new LeafNode(i, freqs[i]));
@@ -53,14 +50,105 @@ public class HuffmanTree {
         }
 
         // 确保树至少有两个叶子节点
-        int index = 0；
-        while (queue.size() < 2) {
-            if (freqs[index] == 0) {
-                queue.add(new LeafNode(index, 1));
+        for (int i = 0; queue.size() < 2; i++) {
+            if (freqs[i] == 0) {
+                queue.add(new LeafNode(i, 1));
             }
-            index++;
         }
 
+        // 初始化树
+        while (queue.size() > 1) {
+            Node left = queue.remove();
+            Node right = queue.remove();
+            queue.add(new InternalNode(left, right));
+        }
+        root = queue.remove();
+
+        // 构造 depthMap
+        traverse(root);
+
+        // 平衡霍夫曼树到限制深度
+        while (maxDepth > limit) {
+            // 在最深的一层获取一个叶子节点(最后一层必然有偶数个叶子节点)
+            LeafNode leafA = depthMap.get(maxDepth).get(0);
+            // 获取 leafA 的父节点
+            InternalNode parentOne = (InternalNode) leafA.parent;
+            LeafNode leafB;
+            if (leafA.side == 0) {
+                leafB = (LeafNode) parentOne.right;
+            } else {
+                leafB = (LeafNode) parentOne.left;
+            }
+            // 使 leafB 代替 parentOne 节点
+            InternalNode parentTwo = (InternalNode) parentOne.parent;
+            if (parentOne.side == 0) {
+                parentTwo.left = leafB;
+                parentTwo.left.parent = parentTwo;
+                parentTwo.left.side = 0;
+            } else {
+                parentTwo.right = leafB;
+                parentTwo.right.parent = parentTwo;
+                parentTwo.side = 1;
+            }
+            // 平衡一次霍夫曼树
+            boolean moved = false;
+            for (int i = maxDepth - 2; i >= 1; i--) {
+                List<LeafNode> leaves = depthMap.get(i);
+                if (Objects.nonNull(leaves)) {
+                    // 在倒数第二层获取一个叶子节点
+                    LeafNode leafC = leaves.get(0);
+                    // 将之前的 leafA 节点和 leafC 组成子树代替 leafC 的节点
+                    InternalNode parentThree = (InternalNode) leafC.parent;
+                    if (leafC.side == 0) {
+                        parentThree.left = new InternalNode(leafA, leafC);
+                        parentThree.left.parent = parentThree;
+                        parentThree.left.side = 0;
+                    } else {
+                        parentThree.right = new InternalNode(leafA, leafC);
+                        parentThree.right.parent = parentThree;
+                        parentThree.right.side = 1;
+                    }
+                    moved = true;
+                    break;
+                }
+            }
+            // 检查是否有重新移动叶子节点
+            if (!moved) {
+                throw new AssertionError("霍夫曼树已不能被再平衡");
+            }
+            // 重新遍历
+            traverse(root);
+        }
+    }
+
+    /**
+     * 从根节点遍历霍夫曼树
+     * @param root 根节点
+     */
+    private void traverse(Node root) {
+        depthMap.clear();
+        maxDepth = 0;
+        traverse(root, 0);
+    }
+
+    /**
+     * 从当前节点遍历霍夫曼树
+     * @param node 节点
+     * @param depth 深度
+     */
+    private void traverse(Node node, int depth) {
+        // 设置霍夫曼树的最大深度
+        maxDepth = depth > maxDepth ? depth : maxDepth;
+        // 递归遍历整个树, 并将叶子节点加入到 depthMap 中
+        if (node instanceof InternalNode) {
+            traverse(((InternalNode) node).left, depth + 1);
+            traverse(((InternalNode) node).right, depth + 1);
+        } else {
+            if (Objects.isNull(depthMap.get(depth))) {
+                depthMap.put(depth, new ArrayList<LeafNode>());
+            }
+            depthMap.get(depth).add((LeafNode) node);
+        }
 
     }
 
